8103099e3f6a7350e2b2f23e153b3a5102ac23ed
Peter Anyaogu <anyaogupeter601@gmail.com>
2022-12-16 19:35:34 +0100

    modules

--- client/components/Cards/PositionCard.tsx
+++ client/components/Cards/PositionCard.tsx
@@ -12,7 +12,7 @@ const PositionCard: FC<IPosition> = (props, { key }) => {
   const { data } = useSession();
   return (
     <div
-      className="card card-compact md:w-72 bg-base-100 md:shadow-xl min-h-[14rem] bg-opacity-50 hover:shadow-md mx-auto"
+      className="card card-compact md:w-72 bg-base-100 md:shadow-xl min-h-[14rem] bg-opacity-50 hover:shadow-md mx-auto w-full"
       key={key}
     >
       <div className="card-body relative">
